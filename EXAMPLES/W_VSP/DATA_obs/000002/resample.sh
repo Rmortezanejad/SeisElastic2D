db3d0dd783950b5d1e7109aa68cae2869b78874f
Wenyong Pan <wpan@ucalgary.ca>
2018-10-10 09:22:54 -0600

    Example

---
+++ EXAMPLES/W_VSP/DATA_obs/000002/resample.sh
@@ -0,0 +1,8 @@
+suoldtonew<Uz_file_single.su>Uz_file_single_new.su
+suresamp<Uz_file_single_new.su>Uz_file_re.su rf=10
+suswapbytes<Uz_file_re.su>Uz_file_single.su
+rm -r Uz_file_single_new.su Uz_file_re.su
+suoldtonew<Ux_file_single.su>Ux_file_single_new.su
+suresamp<Ux_file_single_new.su>Ux_file_re.su rf=10
+suswapbytes<Ux_file_re.su>Ux_file_single.su
+rm -r Ux_file_single_new.su Ux_file_re.su
